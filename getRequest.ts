a57af2edc93bd76752df45264e29beefbfa2fb2c
0verfl0w767 <tkddbs7631@naver.com>
2023-08-08 01:31:54 +0900

    identified the number of minorities

--- getRequest.ts
+++ getRequest.ts
@@ -98,7 +98,11 @@ const getConvert = () => {
           }
         }
 
-        if (department == '공통(교양)' || department == '컴퓨터공학부') infos.push(info)
+        if (
+          (department == '공통(교양)' || department == '컴퓨터공학부') &&
+          (+limit - +sugang == 1 || +limit - +sugang == 2)
+        )
+          infos.push(info)
       }
     }
   }
